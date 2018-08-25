c7c1d411e3005eddc5c24999c25cc833237890dd
PaulEscamilla <35501410+PaulEscamilla@users.noreply.github.com>
2018-08-25 11:43:42 -0500

    Cambio de color del circulo y vida del mismo.

---
+++ unity-sessions-interaction-between-objects-setup/interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int life = 9;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnGUI()
+    {
+        GUILayout.Label("Vida:  " + life);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
